d0690b4a5d4fa7097562c4c69e0a94f47043af17
Olga <olganedelcuam@gmail.com>
2023-02-02 12:20:19 +0100

    ApolloClient library

--- fetching_jpbs/src/components/Jobs.js
+++ fetching_jpbs/src/components/Jobs.js
@@ -1,6 +1,10 @@
-import React from 'react';
+import React from "react";
 import { gql, useQuery } from "@apollo/client";
+import ApolloClient from "apollo-client";
 
+const client = new ApolloClient({
+  uri: "https://graphql.jobs/docs/api/"
+});
 
 const JOBS_QUERY = gql`
   query JobsQuery {
@@ -25,29 +29,31 @@ const Jobs = () => {
   if (error) return <p>There was an error </p>;
 
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Title</th>
-          <th>Company Name</th>
-          <th>City</th>
-          <th>Country</th>
-          <th>Remote</th>
-        </tr>
-      </thead>
-      <tbody>
-        {data.jobs.map((job) => (
-          <tr key={job.id}>
-            <td>{job.title}</td>
-            <td>{job.company.name}</td>
-            <td>{job.location.city}</td>
-            <td>{job.location.country}</td>
-            <td>{job.remote ? 'Yes' : 'No'}</td>
+    <ApolloProvider client={client}>
+      <table>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Company Name</th>
+            <th>City</th>
+            <th>Country</th>
+            <th>Remote</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {data.jobs.map((job) => (
+            <tr key={job.id}>
+              <td>{job.title}</td>
+              <td>{job.company.name}</td>
+              <td>{job.location.city}</td>
+              <td>{job.location.country}</td>
+              <td>{job.remote ? "Yes" : "No"}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </ApolloProvider>
   );
-}
+};
 
 export default Jobs;
